5e1950316e79733cc1a21060abfeee2c04b1bbe4
tungripe <tung@ripegroup.com>
2018-03-07 23:49:14 +1100

    add filter contacts by company id and company name

--- Saasu.API.Client.IntegrationTests/ContactTests.cs
+++ Saasu.API.Client.IntegrationTests/ContactTests.cs
@@ -246,12 +246,12 @@ namespace Saasu.API.Client.IntegrationTests
         public static ProxyResponse<ContactResponse> AssertContactProxy(ContactsProxy proxy, int? pageNumber = null, int? pageSize = null, DateTime? lastModifiedFromDate = null, DateTime? lastModifiedToDate = null,
             string givenName = null, string familyName = null, string organisationName = null,
             bool? isActive = null, bool? isCustomer = null, bool? isSupplier = null, bool? isContractor = null, bool? isPartner = null,
-            string tags = null, string tagSelection = null, string email = null, string contactId = null)
+            string tags = null, string tagSelection = null, string email = null, string contactId = null, string companyName = null, int? companyId = null)
         {
             var response = proxy.GetContacts(pageNumber, pageSize, lastModifiedFromDate, lastModifiedToDate,
                 givenName, familyName, organisationName,
                 isActive, isCustomer, isSupplier, isContractor, isPartner,
-                tags, tagSelection, email, contactId);
+                tags, tagSelection, email, contactId, companyName, companyId);
 
             Assert.IsNotNull(response);
             Assert.IsTrue(response.IsSuccessfull);
